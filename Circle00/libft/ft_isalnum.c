ca96f558264e56dd8c89748db6b392bcc0e4ab41
minsuki2 <minsuki2@student.42seoul.kr>
2021-11-30 20:25:06 +0900

    Changes to be committed: 	renamed:    .ft_isalnum.c.swp -> .Makefile.swp 	modified:   Makefile 	modified:   ft_isalnum.c 	renamed:    u.txt -> ft_isalnum.o 	modified:   ft_isalpha.c 	new file:   ft_isalpha.o 	modified:   ft_isascii.c 	new file:   ft_isdigit.o 	new file:   libft.a 	deleted:    mu.txt 	deleted:    u22.txt

--- Circle00/libft/ft_isalnum.c
+++ Circle00/libft/ft_isalnum.c
@@ -6,13 +6,11 @@
 /*   By: minsuki2 <minsuki2@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 12:14:41 by minsuki2          #+#    #+#             */
-/*   Updated: 2021/11/30 18:17:02 by minsuki2         ###   ########.fr       */
+/*   Updated: 2021/11/30 18:53:38 by minsuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-int	ft_isalpha(int c);
-int ft_isdigit(int c);
 
 int	ft_isalnum(int c)
 {

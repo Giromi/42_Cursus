a2e7caadd64e16b1a1a75f0e551afb3d97a42903
Giromo <giromi@cau.ac.kr>
2021-12-11 05:50:20 +0900

    modified:   Circle00/libft/ft_strlcpy.c 	new file:   Circle00/libft/ft_strlcpy.o 	modified:   Circle00/libft/ft_strlen.c 	deleted:    Circle00/libft/ft_strlen.o 	new file:   Circle00/libft/ft_strlen_const_test.c 	new file:   Circle00/libft/ft_strlen_finished.c

--- Circle00/libft/ft_strlen_const_test.c
+++ Circle00/libft/ft_strlen_const_test.c
@@ -1,16 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_strlen_const_test.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: minsuki2 <minsuki2@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 11:37:03 by minsuki2          #+#    #+#             */
-/*   Updated: 2021/12/11 04:58:39 by minsuki2         ###   ########.fr       */
+/*   Updated: 2021/12/11 05:49:46 by minsuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <string.h>
+#include <stdio.h>
+size_t	ft_strlen(const char *s);
+
+
+int main(void)
+{
+	char src2[2] = "12";
+	char *src3;
+
+
+	/** src2[0] = '1'; */
+	/** src2[1] = '2'; */
+	src3 = &(src2[2]);
+	*(src3) = '4';
+
+	printf("------------strlen------------\n");
+	printf("len  : %zu\n", strlen(src2));
+	printf("len  : %zu\n", ft_strlen(src2));
+	printf("what : %s\n", src2);
+	for (int i = 0; i < 9; i++)
+		printf("[%c]", *(src2 + i));
+	printf("\n");
+	for (int i = 0; i < 9; i++)
+		printf("[%d]", *(src2 + i));
+	printf("\n");
+	return (0);
+}
 
 size_t	ft_strlen(const char *s)
 {
--- Circle00/main.c
+++ Circle00/main.c
@@ -4,70 +4,70 @@
 
 int main(void)
 {
-    int src[] = { 7, 6, 5, 4, 3, 2, 1};
-    int dest[7];
-	char src1[] = "BlockDMask";
-	char str[] = "BlockDMask";
-	char str1[] = "BlockDMask";
-    char dest1[] = "fffffdddddzzzzz";
-    char dest2[] = "fffffdddddzzzzz";
-    char dest3[] = "fffffdddddzzzzz";
-	char mysrc1[] = "BlockDMask";
-    char mydest1[] = "fffffdddddzzzzz";
-    char mydest2[] = "fffffdddddzzzzz";
-    char mydest3[] = "fffffdddddzzzzz";
- 
-    for (int i = 0; i < 7; ++i)
-        printf("[%d]", src[i]);
-    printf("\n");
-    for (int i = 0; i < 7; ++i)
-        printf("[%d]", dest[i]);
-    printf("\n");
-    memmove(dest, src, sizeof(int) * 2);
-
-    for (int i = 0; i < 7; ++i)
-        printf("[%d]", src[i]);
-    printf("\n");
-    for (int i = 0; i < 7; ++i)
-        printf("[%d]", dest[i]);
-    printf("\n");
-
-    printf("----------------------------------------\n");
-    // 메모리 복사1 : src 길이만큼만 복사
-    memmove(dest1, src1, sizeof(char) * 10);
-    ft_memcpy(mydest1, src1, sizeof(char) * 10);
- 
-    // 메모리 복사2 : src 길이 + 1 만큼 복사
-    memmove(dest2, src1, sizeof(char) * 10 + 1);
-    ft_memcpy(mydest2, src1, sizeof(char) * 10 + 1);
- 
-    // 메모리 복사3 : 메모리 일부 복사
-    memmove(dest3 + 10, src1, sizeof(char) * 3);
-    ft_memcpy(mydest3 + 10, src1, sizeof(char) * 3);
-
-    // 메모리 복사4 : 메모리 겹침
-    memmove(str, str + 2, sizeof(char) * 4);
-    ft_memcpy(str1, str1 + 2, sizeof(char) * 4);
-
- 
- 
-    // source
+	const char src1[] = "Hello_World";
+	char re_dst1[12];
+	char my_dst1[12];
 
 
-    // source
-    printf("src1  : %s\n", src1);
-    // destination
-    printf("dest1 : %s\n", dest1);
-    printf("mydest1 : %s\n", mydest1);
-    printf("dest2 : %s\n", dest2);
-    printf("mydest2 : %s\n", mydest2);
-    printf("dest3 : %s\n", dest3);
-    printf("mydest3 : %s\n", mydest3);
+	char src2[5] = "12345";
+	char re_dst2[12];
+	char my_dst2[12];
+	size_t si = 12;
+
+	printf("------strlcpy-----\n");
+	printf("< before >\n");
+	for (int i = 0; i < si; i++) 
+		printf("[%c]", *(re_dst1 + i));
 	printf("\n");
-	printf("ori    : %s\n", src1);
-    printf("src    : %s\n", str);
-    printf("mysrc  : %s\n", str1);
-    printf("long max  : %ld\n", __LONG_MAX__);
- 
-    return 0;
+	for (int i = 0; i < si; i++) 
+		printf("[%c]", *(my_dst1 + i));
+	printf("\n");
+	printf("real : %zu\n", strlcpy(re_dst1, src1, sizeof(char) * 7));
+	printf(" my  : %zu\n", ft_strlcpy(my_dst1, src1, sizeof(char) * 7));
+	
+	printf("< after >\n");
+	for (int i = 0; i < si; i++) 
+		printf("[%c]", *(re_dst1 + i));
+	printf("\n");
+	for (int i = 0; i < si; i++) 
+		printf("[%d]", *(re_dst1 + i));
+	printf("\n");
+	printf("\n");
+	for (int i = 0; i < si; i++) 
+		printf("[%c]", *(my_dst1 + i));
+	printf("\n");
+	for (int i = 0; i < si; i++) 
+		printf("[%d]", *(my_dst1 + i));
+	printf("\n");
+	printf("\n");
+
+	printf("< before >\n");
+	for (int i = 0; i < si; i++) 
+		printf("[%c]", *(re_dst2 + i));
+	printf("\n");
+	for (int i = 0; i < si; i++) 
+		printf("[%c]", *(my_dst2 + i));
+	printf("\n");
+	printf("real : %zu\n", strlcpy(re_dst2, src2, sizeof(char) * 2));
+	printf(" my  : %zu\n", ft_strlcpy(my_dst2, src2, sizeof(char) * 2));
+	printf("< after >\n");
+	for (int i = 0; i < si; i++) 
+		printf("[%c]", *(re_dst2 + i));
+	printf("\n");
+	for (int i = 0; i < si; i++) 
+		printf("[%d]", *(re_dst2 + i));
+	printf("\n");
+	printf("\n");
+	for (int i = 0; i < si; i++) 
+		printf("[%c]", *(my_dst2 + i));
+	printf("\n");
+	for (int i = 0; i < si; i++) 
+		printf("[%d]", *(my_dst2 + i));
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("------------strlen------------\n");
+	printf("len : %zu\n", strlen(src2));
+	printf("len : %zu\n", ft_strlen(src2));
+	return (0);
 }

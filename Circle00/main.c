835e80d783f6aee0b38646a303949082f988e439
Giromo <giromi@cau.ac.kr>
2021-12-15 13:41:59 +0900

    modified:   Circle00/main.c

--- Circle00/main.c
+++ Circle00/main.c
@@ -1,73 +1,35 @@
-#include <string.h>
 #include "libft/libft.h"
 #include <stdio.h>
-
+#include <string.h>
 int main(void)
 {
-	const char src1[] = "Hello_World";
-	char re_dst1[12];
-	char my_dst1[12];
+	char	what1[5] = "Hello";
+	char	what2[6] = "OhYeah";
 
-
-	char src2[5] = "12345";
-	char re_dst2[12];
-	char my_dst2[12];
-	size_t si = 12;
-
-	printf("------strlcpy-----\n");
-	printf("< before >\n");
-	for (int i = 0; i < si; i++) 
-		printf("[%c]", *(re_dst1 + i));
-	printf("\n");
-	for (int i = 0; i < si; i++) 
-		printf("[%c]", *(my_dst1 + i));
-	printf("\n");
-	printf("real : %zu\n", strlcpy(re_dst1, src1, sizeof(char) * 7));
-	printf(" my  : %zu\n", ft_strlcpy(my_dst1, src1, sizeof(char) * 7));
 	
-	printf("< after >\n");
-	for (int i = 0; i < si; i++) 
-		printf("[%c]", *(re_dst1 + i));
+	printf("----------memchr----------\n");
+	printf("ori  : %p\n", what1);
+	printf("find : 'H'\n");
+	printf("real : %p\n", memchr(what1, 'H', 6));
+	printf("my   : %p\n", ft_memchr(what1, 'H', 6));
 	printf("\n");
-	for (int i = 0; i < si; i++) 
-		printf("[%d]", *(re_dst1 + i));
+	printf("ori  : %p\n", what1);
+	printf("find : 'l'\n");
+	printf("real : %p\n", memchr(what1, 'l', 4));
+	printf("my   : %p\n", ft_memchr(what1, 'l', 4));
 	printf("\n");
+	printf("ori  : %s\n", what2);
+	printf("ori  : %p\n", what2);
+	printf("find : 'h'\n");
+	printf("real : %p\n", memchr(what2, 'h', 6));
+	printf("my   : %p\n", ft_memchr(what2, 'h', 6));
 	printf("\n");
-	for (int i = 0; i < si; i++) 
-		printf("[%c]", *(my_dst1 + i));
-	printf("\n");
-	for (int i = 0; i < si; i++) 
-		printf("[%d]", *(my_dst1 + i));
-	printf("\n");
-	printf("\n");
-
-	printf("< before >\n");
-	for (int i = 0; i < si; i++) 
-		printf("[%c]", *(re_dst2 + i));
-	printf("\n");
-	for (int i = 0; i < si; i++) 
-		printf("[%c]", *(my_dst2 + i));
-	printf("\n");
-	printf("real : %zu\n", strlcpy(re_dst2, src2, sizeof(char) * 2));
-	printf(" my  : %zu\n", ft_strlcpy(my_dst2, src2, sizeof(char) * 2));
-	printf("< after >\n");
-	for (int i = 0; i < si; i++) 
-		printf("[%c]", *(re_dst2 + i));
-	printf("\n");
-	for (int i = 0; i < si; i++) 
-		printf("[%d]", *(re_dst2 + i));
+	printf("ori  : %s\n", what2);
+	printf("ori  : %p\n", what2);
+	printf("find : '\\0'\n");
+	printf("real : %p\n", memchr(what1, '\0', 10));
+	printf("my   : %p\n", ft_memchr(what1, '\0', 10));
 	printf("\n");
 	printf("\n");
-	for (int i = 0; i < si; i++) 
-		printf("[%c]", *(my_dst2 + i));
-	printf("\n");
-	for (int i = 0; i < si; i++) 
-		printf("[%d]", *(my_dst2 + i));
-	printf("\n");
-	printf("\n");
-	printf("\n");
-	printf("------------strlen------------\n");
-	printf("len : %zu\n", strlen(src2));
-	printf("len : %zu\n", ft_strlen(src2));
 	return (0);
 }

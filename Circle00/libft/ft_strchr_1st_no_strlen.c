12631acc44cb0c26a68fbd8ee75435fa34c53edf
Giromo <giromi@cau.ac.kr>
2021-12-15 11:31:25 +0900

    modified:   ft_strchr.c 	new file:   ft_strchr_1st_no_strlen.c 	modified:   ft_strrchr.c

--- Circle00/libft/ft_strchr_1st_no_strlen.c
+++ Circle00/libft/ft_strchr_1st_no_strlen.c
@@ -1,20 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_tolower.c                                       :+:      :+:    :+:   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: minsuki2 <minsuki2@student.42seoul.kr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/13 20:48:20 by minsuki2          #+#    #+#             */
-/*   Updated: 2021/12/13 20:48:22 by minsuki2         ###   ########.fr       */
+/*   Created: 2021/12/13 21:49:01 by minsuki2          #+#    #+#             */
+/*   Updated: 2021/12/14 16:59:38 by minsuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_tolower(int c)
+char	*ft_strchr(const char *s, int c)
 {
-	if (c >= 'A' && c <= 'Z')
-		return (c + 32);
-	return (c);
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return (&((char *)s)[i]);
+		i++;
+	}
+	if ( (char)c == '\0')
+		return (&((char *)s)[i]);
+	return (NULL);
 }

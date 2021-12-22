0098162deb6eeadc8e087422f1b46f56324ea0c6
Giromo <giromi@cau.ac.kr>
2021-12-22 15:54:28 +0900

    modified:   ft_strlcat.c 	new file:   ft_strlcat_1st_two_return.c 	new file:   ft_strlcat_2nd_return_just_one.c

--- Circle00/libft/ft_strlcat_1st_two_return.c
+++ Circle00/libft/ft_strlcat_1st_two_return.c
@@ -6,7 +6,7 @@
 /*   By: minsuki2 <minsuki2@student.42seoul.kr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 13:43:32 by minsuki2          #+#    #+#             */
-/*   Updated: 2021/12/15 22:48:22 by minsuki2         ###   ########.fr       */
+/*   Updated: 2021/12/15 18:46:30 by minsuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,20 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	dstLen;
 	size_t	srcLen;
-	size_t	offsetLen;
+	size_t	i;
 
 	dstLen = ft_strlen(dst);
 	srcLen = ft_strlen(src);
-	offsetLen = 0;
-	if (dstLen + 1 <= dstsize)
+	i = 0;
+	if (dstsize >= dstLen + 1)
 	{
-		while (dstLen + offsetLen < dstsize && src[offsetLen - 1])
+		while (i + dstLen < dstsize && src[i])
 		{
-			dst[dstLen + offsetLen - 1] = src[offsetLen];
-			offsetLen++;
+			dst[i + dstLen] = src[i];
+			i++;
 		}
-		dst[(dstLen + offsetLen) - 1] = '\0';
+		dst[i + dstLen - 1] = '\0';
+		return (dstLen + srcLen);
 	}
-	else
-		dstLen = dstsize;
-	return (dstLen + srcLen);
+	return (dstsize + srcLen);
 }
-
--- Circle00/libft/ft_strlcat_2nd_return_just_one.c
+++ Circle00/libft/ft_strlcat_2nd_return_just_one.c
@@ -6,7 +6,7 @@
 /*   By: minsuki2 <minsuki2@student.42seoul.kr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 13:43:32 by minsuki2          #+#    #+#             */
-/*   Updated: 2021/12/15 22:48:22 by minsuki2         ###   ########.fr       */
+/*   Updated: 2021/12/15 18:40:55 by minsuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,19 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	dstLen;
 	size_t	srcLen;
-	size_t	offsetLen;
+	size_t	i;
 
 	dstLen = ft_strlen(dst);
 	srcLen = ft_strlen(src);
-	offsetLen = 0;
-	if (dstLen + 1 <= dstsize)
+	i = 0;
+	if (dstLen <= dstsize - 1)
 	{
-		while (dstLen + offsetLen < dstsize && src[offsetLen - 1])
+		while (i + dstLen < dstsize && src[i])
 		{
-			dst[dstLen + offsetLen - 1] = src[offsetLen];
-			offsetLen++;
+			dst[i + dstLen] = src[i];
+			i++;
 		}
-		dst[(dstLen + offsetLen) - 1] = '\0';
+		dst[i + dstLen - 1] = '\0';
 	}
 	else
 		dstLen = dstsize;

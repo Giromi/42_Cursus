f17f434c1bb8c2e8d2dbebdf0fd5d87289ba2d5a
minsuki2 <minsuki2@student.42seoul.kr>
2022-01-25 15:28:09 +0900

    Changes to be committed: 	new file:   main.c

---
+++ Circle01/main.c
@@ -0,0 +1,37 @@
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "get_next_line.h"
+
+int main()
+{
+	int             fd;
+	int             i;
+	int             j;
+	char    		*line = 0;
+	char			*lineadress[66];
+
+	j = 1;
+	if (!(fd = open("check.txt", O_RDONLY)))
+	{
+		printf("\nError in open\n");
+		return (0);
+	}
+	while ((i = get_next_line(fd, &line)) > 0)
+	{
+		printf("%s\n", line);
+		lineadress[j - 1] = line;
+		j++;
+	}
+	printf("%s\n", line);
+	free(line);
+	close(fd);
+
+	while (--j > 0)
+		free(lineadress[j - 1]);
+
+	return (0);
+}

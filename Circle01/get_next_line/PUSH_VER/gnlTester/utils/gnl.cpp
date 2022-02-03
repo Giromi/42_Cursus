662753aead0ac7478f7d6874b4de56320ceceb26
Giromo <giromi@cau.ac.kr>
2022-02-03 10:52:46 +0900

    gnl_mandatory_tester

---
+++ Circle01/get_next_line/PUSH_VER/gnlTester/utils/gnl.cpp
@@ -0,0 +1,36 @@
+extern "C"
+{
+#define new tripouille
+#include "get_next_line.h"
+#undef new
+}
+
+#include "gnl.hpp"
+#include "color.hpp"
+#include "check.hpp"
+
+#include <string.h>
+
+extern int iTest;
+
+using namespace std;
+
+void gnl(int fd, char const * expectedReturn)
+{
+	char	*gnlReturn = get_next_line(fd);
+	if (expectedReturn == NULL)
+	{
+		// printf("\n");
+		check(gnlReturn == NULL);
+		// printf("\nmy : %s\nre : %s\n", gnlReturn, expectedReturn);
+	}
+	else
+	{
+		printf("\n");
+		check(!strcmp(gnlReturn, expectedReturn));
+		printf("\nmy : %s\nre : %s\n", gnlReturn, expectedReturn);
+	printf("\n");
+	}
+	free(gnlReturn);
+	iTest++;
+}
